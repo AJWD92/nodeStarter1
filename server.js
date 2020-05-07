5fef6b30e8ea9ef7a1cff08f18ddcf74db8b1163
Aaron-Doran <wesley.gossett@gmail.com>
2020-05-07 13:57:39 -0400

    Initial commit forget to do this at the beginning so already have a base set up for learing http project

---
+++ server.js
@@ -0,0 +1,35 @@
+const http = require('http');
+
+const todos = [
+    {
+        id: 1,
+        text: 'Todo One'
+    },
+    {
+        id: 2,
+        text: 'Todo Two'
+    },
+    {
+        id: 3,
+        text: 'Todo Three'
+    },
+];
+
+const server = http.createServer((req, res) => {
+    res.writeHead(400, {
+        'Content-Type': 'application/json',
+        'X-Powered-By': 'Node.js'
+    });
+
+    res.end(
+        JSON.stringify({
+            success: false,
+            error: 'Please add email or password in correct field',
+            data: null
+        })
+    );
+})
+
+const PORT = 4000;
+
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
